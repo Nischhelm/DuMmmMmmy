5cea74edc3bfe659f8ab46b23444ee0565aa324f
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-09-21 13:43:21 +0200

    no lithium shit

--- fabric/src/main/java/net/mehvahdjukaar/dummmmmmy/fabric/DummmmmmyFabric.java
+++ fabric/src/main/java/net/mehvahdjukaar/dummmmmmy/fabric/DummmmmmyFabric.java
@@ -1,12 +1,10 @@
 package net.mehvahdjukaar.dummmmmmy.fabric;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.mehvahdjukaar.dummmmmmy.Dummmmmmy;
 import net.mehvahdjukaar.dummmmmmy.common.ModEvents;
 import net.mehvahdjukaar.moonlight.api.platform.PlatHelper;
-import net.mehvahdjukaar.moonlight.api.platform.fabric.ClientHelperImpl;
 
 public class DummmmmmyFabric implements ModInitializer {
 
@@ -15,16 +13,15 @@ public class DummmmmmyFabric implements ModInitializer {
         Dummmmmmy.init();
 
         if (PlatHelper.isModLoaded("lithium")) {
-            Dummmmmmy.LOGGER.warn("Lithium detected. MmmMmmMmmMmm scarecrow mode has been disabled as lithium doesnt have a way to add goals to new entities");
-        } else {
-            //TODO: fix this causing concurrency issues
-           ServerEntityEvents.ENTITY_LOAD.register((e, w) -> {
-               try {
-                   ModEvents.onEntityJoinWorld(e);
-               } catch (Exception ex) {
-                   Dummmmmmy.LOGGER.error("Error adding scarecrow goal to entity", ex);
-               }
-           });
+            Dummmmmmy.LOGGER.warn("Lithium detected. MmmMmmMmmMmm scarecrow mode might not work well due to a lithium bug (the mod doesnt have a way to add goals to new entities)");
         }
+        ServerEntityEvents.ENTITY_LOAD.register((e, w) -> {
+            try {
+                ModEvents.onEntityJoinWorld(e);
+            } catch (Exception ex) {
+                Dummmmmmy.LOGGER.error("Error adding scarecrow goal to entity", ex);
+            }
+        });
+
     }
 }
